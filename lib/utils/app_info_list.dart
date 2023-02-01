d5596d9fbbcbc167806e8754a41fc4821aab87f9
chetryJyoti <jyotichetry087@gmail.com>
2023-02-01 23:27:33 +0530

    added dummy data for tickets

---
+++ lib/utils/app_info_list.dart
@@ -0,0 +1,51 @@
+List<Map<String, dynamic>> hotelList = [
+  {
+    'img': 'one.png',
+    'place': 'Open Space',
+    'destination': 'Assam',
+    'price': 39
+  },
+  {
+    'img': 'two.png',
+    'place': 'Global Wall',
+    'destination': 'Nepal',
+    'price': 98
+  },
+  {
+    'img': 'three.png',
+    'place': 'Tallest Building',
+    'destination': 'Dubai',
+    'price': 48
+  },
+];
+
+List<Map<String, dynamic>> ticketList = [
+  {
+    'from': {
+      'code':"NYC",
+      'name':"New-York"
+    },
+    'to': {
+      'code':"LDN",
+      'name':"London"
+    },
+    'flying_time': '8H 30M',
+    'date': "1 MAY",
+    'departure_time':"08:00 AM",
+    "number":23
+  },
+  {
+    'from': {
+      'code':"DK",
+      'name':"Dhaka"
+    },
+    'to': {
+      'code':"SH",
+      'name':"Shanghai"
+    },
+    'flying_time': '4H 20M',
+    'date': "10 MAY",
+    'departure_time':"09:00 AM",
+    "number":45
+  },
+];
